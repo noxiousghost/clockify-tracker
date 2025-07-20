6c516cc286c4ff13fec977eac34c6dc7905a59d7
Sajan Rajbhandari <sazanrjb@gmail.com>
2025-07-20 14:03:30 +0545

    chore: change folder

--- scripts/db-cleanup.js
+++
@@ -1,7 +0,0 @@
-import { deleteOldSessions } from '../dist/lib/db.js';
-
-const days = process.argv[2] ? parseInt(process.argv[2], 10) : 5;
-
-deleteOldSessions(days);
-
-console.log(`Deleted sessions older than ${days} days.`);
